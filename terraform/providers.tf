117763f7afcbb4e3052ed506d6390f4c64b7d492
Purvasha <purvasha1013@gmail.com>
2024-10-02 21:39:05 +1000

    adding terraform

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,16 +2,9 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.71.0"
+      version = "~> 4.3.0"
     }
   }
 
   required_version = ">= 1.5.6"
 }
-
-provider "azurerm" {
-  features {}
-  skip_provider_registration = true
-  
-}
-
